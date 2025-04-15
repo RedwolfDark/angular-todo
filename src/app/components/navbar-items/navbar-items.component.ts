56e8752362b3bd7d18c2a1f4e893a3042076be5a
RedwolfDark <30651692+RedwolfDark@users.noreply.github.com>
2025-04-15 22:45:32 +0100

    INIT: initialize the project

---
+++ src/app/components/navbar-items/navbar-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-items',
+  imports: [],
+  templateUrl: './navbar-items.component.html',
+  styleUrl: './navbar-items.component.scss'
+})
+export class NavbarItemsComponent {
+
+}
---
+++ src/app/features/tasks/delete-tasks/delete-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-tasks',
+  imports: [],
+  templateUrl: './delete-tasks.component.html',
+  styleUrl: './delete-tasks.component.scss'
+})
+export class DeleteTasksComponent {
+
+}
